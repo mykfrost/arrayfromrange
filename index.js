621e3e2d5b70e0dae0281f2c5f1046d7d1450b0f
Michael Okello <okellofrost@gmail.com>
2022-07-12 22:36:58 +0300

    Arry from range 2

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function arrayFromRange(min , max){
   output.push(i);
   return output;
 }
+
+// Includes
+ const numbers2 = [1,2,3,4];
+// array to check if an element exists in an array
+
+//check here
+ console.log(includes(numbers2, 3));
+
+ function includes(array , searchElement){
+ 
+  for (let element of array)
+if (element === searchElement){
+     return true;
+}
+    return false;
+ }
