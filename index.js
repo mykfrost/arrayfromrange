13fc4963852c1e48ccd6d6dfc8d402fe2ec2fe75
Michael Okello <okellofrost@gmail.com>
2022-07-13 11:22:02 +0300

    Exludes operations

--- index.js
+++ index.js
@@ -19,8 +19,22 @@ function arrayFromRange(min , max){
  function includes(array , searchElement){
  
   for (let element of array)
-if (element === searchElement){
+      if (element === searchElement)
      return true;
-}
+
     return false;
+ }
+
+
+ const numbers3 = [1,2,3,4,5,6,7,8,9];
+const output2 = except(numbers3,[1,2,3,4]);
+ console.log(output2);
+
+ function except (array , excluded){
+ const output2 = [];
+
+  for (let element of array)
+    if (!excluded.includes(element))//checking to see if the current element is excluded in the array
+   output2.push(element);
+     return output2;
  }
